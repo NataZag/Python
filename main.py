a2c09d1846861784abe31acd143c2512ac1e05a4
Kisel-Zagoryanskaya Natallia <91523843+NataZag@users.noreply.github.com>
2021-10-26 13:53:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+name = input ('Input your name: ')
+print ('Hello,', name)
+name = 'Natasha'
+name_len = len(name)
+print('Name:', name)
+print('Letters:', name_len)
+
+#Сортировка
+sorted_name = sorted(name)
+print('Sorted name:', sorted_name)#сортирует по алфавиту, регистрозависим
+
+#Тип данных
+name_type = type(name)
+print('Name type:', name_type)
