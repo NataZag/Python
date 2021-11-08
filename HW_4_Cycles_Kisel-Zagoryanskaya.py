ad845698282b68b1a47320752b6a7d415f244433
Kisel-Zagoryanskaya Natallia <91523843+NataZag@users.noreply.github.com>
2021-11-08 13:03:31 +0300

    Phyton_HW_4

--- HW_4_Cycles_Kisel-Zagoryanskaya.py
+++ HW_4_Cycles_Kisel-Zagoryanskaya.py
@@ -73,16 +73,16 @@ run = True
 # 11.3 Сделать if с условием, если item равен  10, то вывести в консоль (‘Item =’, item).
 # 11.4 Сделать if с условием, если item меньше 4, то вывести в консоль (‘Item <’, item).
 # 11.5 Сделать if с условием, если item больше или равно 27, то вывести в консоль (‘Item >=’, item).
-for item in range(0, 30):
-	print('Step =', item)
-	if item == 5:
-		print('Item =', item)
-	if item == 10:
-		print('Item =', item)
-	if item < 4:
-		print('Item <', item)
-	if item >= 27:
-		print('Item >=', item)
+# for item in range(0, 30):
+# 	print('Step =', item)
+# 	if item == 5:
+# 		print('Item =', item)
+# 	if item == 10:
+# 		print('Item =', item)
+# 	if item < 4:
+# 		print('Item <', item)
+# 	if item >= 27:
+# 		print('Item >=', item)
 
 # 12.Сделать цикл for c переменной item который будет работать пока счётчик range досчитает от 0 до range_count +1
 # Тело цикла:
@@ -95,7 +95,17 @@ for item in range(0, 30):
 # 		-- Вывести в консоль (‘-------- Inner_Step =’, inner_item)
 # 		-- Сделать if если inner_item равен 5, то в inner_count присвоить inner_item.
 # 	- За пределами тела предыдущего цикла вывести в консоль (‘-- inner_count =’, inner_count)
-#
+# for item in range(0, range_count + 1):
+# 	print('Step =', item)
+# 	if item == 7:
+# 		inner_count = 0
+# 		print('-- inner_count =', inner_count)
+# 		for inner_item in range(0, item):
+# 			print('-------- Inner_Step =', inner_item)
+# 			if inner_item == 5:
+# 				inner_count = inner_item
+# print('-- inner_count =', inner_count)
+
 # 13.Сделать цикл for c переменной item который будет работать пока счётчик range досчитает от 0 до 20
 # Тело цикла:
 # 13.1 Вывести в консоль (‘Step =’, item)
