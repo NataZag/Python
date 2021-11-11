cf1525a3bfe7a68983788d03ef3f94e9fff47887
Kisel-Zagoryanskaya Natallia <91523843+NataZag@users.noreply.github.com>
2021-11-11 09:59:06 +0300

    Phyton_HW_3

--- HW_3_IfElse_Kisel-Zagoryanskaya.py
+++ HW_3_IfElse_Kisel-Zagoryanskaya.py
@@ -101,36 +101,11 @@ else:
     print('Переменная item_4 = ', item_4)
 
 # 29. Создать переменную currency_convertor со значением item_2
+currency_convertor = item_2
+
 # 30. Сделать if в котором будет условие:
 # если currency_convertor, то выполнять следующие шаги задания,
 # иначе, вывести в консоль (“Переменная currency_convertor = ”, item_3)
-currency_convertor = item_2
-try:
-    if currency_convertor:
-        currency_usd = usd_item
-        target_currency = byn_item
-        target_currency_amount = 50
-        currency_result = 0
-    if target_currency == 'eur':
-        currency_result = target_currency_amount / usd_eur_rate
-        print(target_currency_amount, eur_item, '=', currency_result, usd_item)
-    elif target_currency == 'uah':
-        currency_result = target_currency_amount / usd_uah_rate
-        print(target_currency_amount, uah_item, '=', currency_result, usd_item)
-    elif target_currency == 'chf':
-        currency_result = target_currency_amount / usd_chf_rate
-        print(target_currency_amount, chf_item, '=', currency_result, usd_item)
-    elif target_currency == 'rub':
-        currency_result = target_currency_amount / usd_rub_rate
-        print(target_currency_amount, rub_item, '=', currency_result, usd_item)
-    elif target_currency == 'byn':
-        currency_result = target_currency_amount / usd_byn_rate
-        print(target_currency_amount, byn_item, '=', currency_result, usd_item)
-    else:
-        print('Переменная currency_convertor = ', item_3)
-except NameError:
-    print('Unknow currency')
-
 
 # 31. Внутри if currency_convertor сделать следующие If условия :
 
@@ -156,3 +131,28 @@ except NameError:
 # 31.6 Сделать elif с остальными валютами
 
 # 31.7 Последним оставить else, при выполнений которого в консоль выведется (“Unknow currency”)
+
+if currency_convertor:
+    currency_usd = usd_item
+    target_currency = byn_item
+    target_currency_amount = 50
+    currency_result = 0
+else:
+    print('Переменная currency_convertor = ', item_3)
+if target_currency == 'eur':
+    currency_result = target_currency_amount / usd_eur_rate
+    print(target_currency_amount, eur_item, '=', currency_result, usd_item)
+elif target_currency == 'uah':
+    currency_result = target_currency_amount / usd_uah_rate
+    print(target_currency_amount, uah_item, '=', currency_result, usd_item)
+elif target_currency == 'chf':
+    currency_result = target_currency_amount / usd_chf_rate
+    print(target_currency_amount, chf_item, '=', currency_result, usd_item)
+elif target_currency == 'rub':
+    currency_result = target_currency_amount / usd_rub_rate
+    print(target_currency_amount, rub_item, '=', currency_result, usd_item)
+elif target_currency == 'byn':
+    currency_result = target_currency_amount / usd_byn_rate
+    print(target_currency_amount, byn_item, '=', currency_result, usd_item)
+else:
+    print('Unknow currency')
